cf514494a00065599eaf098a3e27e1c9643cff44
bayaskin <bayas.edu.ne@gmail.com>
2021-01-27 16:45:45 -0600

    Second iteration

--- password_validation.py
+++ password_validation.py
@@ -3,11 +3,14 @@ import re
 
 def validate(passwd):
     if len(passwd) < 8:
-        return("The password is not accepted")
+        return("The password is not accepted, it is long enough")
+
     elif re.search('[0-9]', passwd) is None:
-        return("The password is not accepted")
+        return("The password is not accepted, it must have at least one number")
+
     elif re.search('[a-z]', passwd) is None:
-        return("The password is not accepted")
+        return("The password is not accepted, it must have at least one letter")
+
     else:
         return("The password is accepted")
 
@@ -15,4 +18,4 @@ def validate(passwd):
 
 if __name__ == '__main__':
     passwd = input("Enter a password: ")
-    validate(passwd)
+    print(validate(passwd))
