55ad004b10e970c2876e23b0e66c73bbf3360998
bayaskin <bayas.edu.ne@gmail.com>
2021-01-27 21:08:51 -0600

    third iteration

--- password_validation.py
+++ password_validation.py
@@ -1,21 +1,40 @@
 import re
 
 
-def validate(passwd):
-    if len(passwd) < 8:
-        return("The password is not accepted, it is long enough")
+def validate(passwd, role):
+    if role == 'regular':
 
-    elif re.search('[0-9]', passwd) is None:
-        return("The password is not accepted, it must have at least one number")
+        if len(passwd) < 8:
+            return("The password is not accepted, it is not long enough")
 
-    elif re.search('[a-z]', passwd) is None:
-        return("The password is not accepted, it must have at least one letter")
+        elif re.search('[0-9]', passwd) is None:
+            return("The password is not accepted, it must have at least one number")
 
+        elif re.search('[a-z]', passwd) is None:
+            return("The password is not accepted, it must have at least one letter")
+
+        else:
+            return("The password is accepted")
     else:
-        return("The password is accepted")
+        if len(passwd) < 13:
+            return("The password is not accepted, it is not long enough")
+
+        elif re.search('[0-9]', passwd) is None:
+            return("The password is not accepted, it must have at least one number")
+
+        elif re.search('[a-z]', passwd) is None:
+            return("The password is not accepted, it must have at least one letter")
+
+        elif re.search('[!@#$%^&*]{3}', passwd) is None:
+            return("The password is not accepted, it must have at least one special symbol: '!', '@', '#', '$', '%', '^', '&', or '*'")
+
+        else:
+            return("The password is accepted")
+
 
 
 
 if __name__ == '__main__':
+    role = input("Enter your role (regular, admin): ")
     passwd = input("Enter a password: ")
-    print(validate(passwd))
+    print(validate(passwd, role))
--- test_password_validation.py
+++ test_password_validation.py
@@ -1,23 +1,53 @@
 import pytest
 from .password_validation import validate
 
-def test_short():
+def test_short_regular():
     passwd = '1234'
-    validation = validate(passwd)
+    role = 'regular'
+    validation = validate(passwd, role)
     assert 'The password is not accepted' in validation
 
-def test_only_digit():
+def test_only_digit_regular():
 	passwd = '12345678'
-	validation = validate(passwd)
+	role = 'regular'
+	validation = validate(passwd, role)
 	assert 'The password is not accepted' in validation
 
-def test_only_letters():
+def test_only_letters_regular():
 	passwd = 'jhdfgjdhghd'
-	validation = validate(passwd)
+	role = 'regular'
+	validation = validate(passwd, role)
 	assert 'The password is not accepted' in validation
 
-def test_correct_passwd():
+def test_correct_passwd_regular():
 	passwd = 'password123'
-	validation = validate(passwd)
+	role = 'regular'
+	validation = validate(passwd, role)
 	assert 'The password is accepted' in validation
 
+
+def test_short_admin():
+    passwd = '1qwertypas!'
+    role = 'admin'
+    validation = validate(passwd, role)
+    assert 'The password is not accepted' in validation
+
+def test_only_digit_admin():
+	passwd = '12345678'
+	role = 'admin'
+	validation = validate(passwd, role)
+	assert 'The password is not accepted' in validation
+
+def test_only_letters_admin():
+	passwd = 'jhdfgjdhghd'
+	role = 'admin'
+	validation = validate(passwd, role)
+	assert 'The password is not accepted' in validation
+
+def test_correct_passwd_admin():
+	passwd = '!qwertypassword123'
+	role = 'admin'
+	validation = validate(passwd, role)
+	assert 'The password is accepted' in validation
+
+	
